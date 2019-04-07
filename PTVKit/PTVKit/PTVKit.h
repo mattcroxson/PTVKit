5d350975dd719666bc9912598ee3f3d40534df6f
Matt Croxson <mattcroxson@gmail.com>
2019-04-07 15:02:59 +1000

    Added Xcode project

---
+++ PTVKit/PTVKit/PTVKit.h
@@ -0,0 +1,19 @@
+//
+//  PTVKit.h
+//  PTVKit
+//
+//  Created by Matt Croxson on 7/4/19.
+//  Copyright © 2019 Matt Croxson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PTVKit.
+FOUNDATION_EXPORT double PTVKitVersionNumber;
+
+//! Project version string for PTVKit.
+FOUNDATION_EXPORT const unsigned char PTVKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PTVKit/PublicHeader.h>
+
+

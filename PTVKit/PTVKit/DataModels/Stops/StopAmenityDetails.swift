985a8ff4af13a8ef0d14376b340e22edf8c36667
Matt Croxson <mattcroxson@gmail.com>
2019-04-07 17:12:19 +1000

    Added stop amenity details

---
+++ PTVKit/PTVKit/DataModels/Stops/StopAmenityDetails.swift
@@ -0,0 +1,25 @@
+//
+//  StopAmenityDetails.swift
+//  PTVKit
+//
+//  Created by Matt Croxson on 7/4/19.
+//  Copyright © 2019 Matt Croxson. All rights reserved.
+//
+
+import Foundation
+
+/// Represents amenity and facility details at the stop
+public struct StopAmenityDetails: Decodable {
+
+    /// Indicates if there is a public toilet at or near the stop
+    public let toilet: Bool?
+
+    /// Indicates if there is a taxi rank at or near the stop
+    public let taxiRank: Bool?
+
+    /// The number of free car parking spots at the stop
+    public let carParking: Bool?
+
+    /// Indicates if there are CCTV (i.e. closed circuit television) cameras at the stop
+    public let cctv: Bool?
+}

54727f562d7e9a3fcdf0c3a486dc8f885c1279ce
Matt Croxson <mattcroxson@gmail.com>
2019-04-07 15:33:47 +1000

    Added Route Types and Status

---
+++ PTVKit/PTVKit/DataModels/Status.swift
@@ -0,0 +1,23 @@
+//
+//  Status.swift
+//  PTVKit
+//
+//  Created by Matt Croxson on 7/4/19.
+//  Copyright © 2019 Matt Croxson. All rights reserved.
+//
+
+import Foundation
+
+/// API system health status
+public struct Status: Decodable {
+
+    /// API version number
+    public let version: String?
+
+    /// API system health
+    ///
+    /// The API will return one of the two below possible responses:
+    /// - `0` = offline
+    /// - `1` = online
+    public let health: Int?
+}

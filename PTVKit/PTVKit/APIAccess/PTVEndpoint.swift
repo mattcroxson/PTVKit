77855c98ca24023697837ce1bf4ba0d217a3b01a
Matt Croxson <mattcroxson@gmail.com>
2019-04-25 09:47:16 +1000

    Added disruption endpoints

--- PTVKit/PTVKit/APIAccess/PTVEndpoint.swift
+++ PTVKit/PTVKit/APIAccess/PTVEndpoint.swift
@@ -17,15 +17,33 @@ internal enum PTVEndpoint: PTVEndpointConfigurer {
     case stopsByLocation(latitude: Double, longitude: Double)
     case stopsOnRoute(routeId: Int, routeType: Int)
     case stop(stopId: Int, routeType: Int)
+    case disruptions
+    case disruptionsByRoute(routeId: Int)
+    case disruptionsByRouteAndStop(routeId: Int, stopId: Int)
+    case disruptionsByStop(stopId: Int)
+    case disruption(disruptionId: Int)
+    case disruptionModes
 
     var path: String {
         switch self {
+
+        // MARK: Routes
         case let .route(routeId): return "/\(version)/routes/\(routeId)"
         case .routes: return "/\(version)/routes"
         case .routeTypes: return "/\(version)/route_types"
+
+        // MARK: Stops
         case let .stopsByLocation(latitude, longitude): return "/\(version)/stops/location/\(latitude),\(longitude)"
         case let .stopsOnRoute(routeId, routeType): return "/\(version)/stops/route/\(routeId)/route_type/\(routeType)"
         case let .stop(stopId, routeType): return "/\(version)/stops/\(stopId)/route_type/\(routeType)"
+
+        // MARK: Disruptions
+        case .disruptions: return "/\(version)/disruptions/"
+        case let .disruptionsByRoute(routeId): return "/\(version)/disruptions/route/\(routeId)"
+        case let .disruptionsByRouteAndStop(routeId, stopId): return "/\(version)/disruptions/route/\(routeId)/stop/\(stopId)"
+        case let .disruptionsByStop(stopId): return "/\(version)/disruptions/stop/\(stopId)"
+        case let .disruption(disruptionId): return   "/\(version)/disruptions/\(disruptionId)"
+        case .disruptionModes: return "/\(version)/disruptions/modes"
         }
     }
 }
